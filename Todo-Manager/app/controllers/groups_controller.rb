a73a1be5b41548849d1783a6f9873ed8fe6e7226
Yang Shuqi <shu7yang@gmail.com>
2018-01-27 21:59:38 +0800

    change navbar

--- Todo-Manager/app/controllers/groups_controller.rb
+++ Todo-Manager/app/controllers/groups_controller.rb
@@ -23,9 +23,9 @@ class GroupsController < ApplicationController
     
     def create
         @group = current_user.groups.build(group_params)
-        #@group.user = current_user
+        @group.user = current_user
         if @group.save
-            redirect_to :action => :index
+            redirect_to root_path
         else
             render :action => :new
         end
